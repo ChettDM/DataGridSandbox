a5278b44d9597db9957bcd9fe9c7b1227de5c80f
Chett Michael <Chett.Michael@gmail.com>
2019-09-24 16:29:57 -0500

    Initial Commit

---
+++ DataGridSandBox/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DataGridSandBox.Models;
+
+namespace DataGridSandBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Element> List { get; set; }
+
+        public MainWindow()
+        {
+            List = new ObservableCollection<Element>();
+            
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            for (var i = 1; i <= 5; i++)
+                List.Add(new Element
+                {
+                    Id = i,
+                    IdHasError = true,
+                    IdErrorMessage = "Error: This is an example Id Error Message - " + i,
+                    Order = i,
+                    OrderHasError = true,
+                    OrderErrorMessage = "Error: This is an example OrderErrorMessage Error Message - " + i,
+                    Description1 = nameof(Element.Description1) + " - " + i,
+                    Description1HasError = true,
+                    Description1ErrorMessage = "Error: This is an example Description1 Error Message - " + i,
+                    Description2 = nameof(Element.Description2) + " - " + i,
+                    Description2HasError = true,
+                    Description2ErrorMessage = "Error: This is an example Description2 Error Message - " + i,
+
+                    ElementErrorMessage = "Error: This is an example of a row error."
+                });
+
+        }
+    }
+}
---
+++ DataGridSandBox/Models/Element.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridSandBox.Models
+{
+    public class Element
+    {
+        public int Id { get; set; }
+        public bool IdHasError { get; set; }
+        public string IdErrorMessage { get; set; }
+        public double Order { get; set; }
+        public bool OrderHasError { get; set; }
+        public string OrderErrorMessage { get; set; }
+        public string Description1 { get; set; }
+        public bool Description1HasError { get; set; }
+        public string Description1ErrorMessage { get; set; }
+        public string Description2 { get; set; }
+        public bool Description2HasError { get; set; }
+        public string Description2ErrorMessage { get; set; }
+
+
+        public string ElementErrorMessage { get; set; }
+    }
+}
